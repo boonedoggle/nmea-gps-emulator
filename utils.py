8eccdabc625b4d75e338c27bd6ab8fbdd2f023a2
deepwave <deepwave@moffatt.phl.deepwavedigital.com>
2026-09-11 11:14:11 -0400

    Avoid removing old web service during install

--- utils.py
+++ utils.py
@@ -19,20 +19,8 @@ def remove_legacy_gpsd_override():
             os.remove(path)
 
 
-def remove_legacy_web_service():
-    path = '/etc/systemd/system/nmea_gps_web.service'
-    if not os.path.isfile(path):
-        return
-    with open(path) as stream:
-        contents = stream.read()
-    if 'nmea_gps_emulator.web' in contents:
-        subprocess.run(['systemctl', 'disable', '--now', 'nmea_gps_web.service'], check=False)
-        os.remove(path)
-
-
 def add_system_services():
     remove_legacy_gpsd_override()
-    remove_legacy_web_service()
     services_files = list()
     services_files += glob.glob('services/*.service')
     for service_file in services_files:
